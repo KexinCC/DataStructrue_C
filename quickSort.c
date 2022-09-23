84d12990eba6c7bfddb803658b56124d682ceac1
KexinCC <kevin_czm@qq.com>
2022-09-23 23:06:45 +0800

    quickSort

--- quickSort.c
+++ quickSort.c
@@ -4,17 +4,46 @@
 
 #include <stdio.h>
 
-void printArray(int array[], int length) {
-    for (int i = 0; i < length; ++i) {
-        printf("%d ", array[i]);
+void printArray(int array[], int i, int j) {
+    for (int k = 0; k < j - i + 1; ++k) {
+        printf("%d ", array[k]);
     }
     printf("\n");
 }
 
-void quickSort(int array[], int length) {
+int partition(int array[], int i, int j) {
+    int x = array[i];
+    while (i < j) {
+        while (i < j && array[j] >= x) {
+            j--;
+        }
+        if (i < j) {
+            array[i] = array[j];
+            i++;
+        }
+        while (i > j && array[i] < x) {
+            i++;
+        }
+        if (i < j) {
+            array[j] = array[i];
+            j--;
+        }
+    }
+    array[i] = x;
+    return i;
+}
 
+void quickSort(int array[], int i, int j) {
+    if (i < j) {
+        int index = partition(array, i, j);
+        quickSort(array, i, index - 1);
+        quickSort(array, index + 1, j);
+    }
 }
 
 int main() {
-
+    int array[8] = {27, 38, 13, 49, 76, 97, 65, 49};
+    quickSort(array, 0, 7);
+    printArray(array, 0, 7);
+    return 0;
 }
